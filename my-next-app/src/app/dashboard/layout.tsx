3da5da0f221e047a68a07447bce0ca06035b363d
yunyun <imnp516@gmail.com>
2025-12-09 21:02:51 +0700

    fix dashboard

--- my-next-app/src/app/dashboard/layout.tsx
+++ my-next-app/src/app/dashboard/layout.tsx
@@ -11,67 +11,15 @@ interface DashboardLayout {
 function DashboardLayout({ children }: DashboardLayout) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      
-      <Navbar/>
-
-        <header className ="chat-navbar">
-            <h2 className="chat-tite">TIÊU ĐỀ CHAT</h2>
-            <button className="btn-logout">THOÁT TÀI KHOẢN</button>
-        </header>
-
-
-      <div className="main-content-wrapper">
-
-        <div className="left-content"> 
-
-          {children} 
-
-           <div className="left-input">
-              <input className="input-style" placeholder="NHẬP"/>
-           </div>
-
-
-        </div>
-
-
-
-        <div className="right-sidebar">
-
-         <div className="note-wrapper">
-        
-                <div className="header-note">
-                   <h4 className="right-note">GHI CHÚ</h4>
-                   <input className="note-search" placeholder="TÌM KIẾM"/>
-                   <button  className="note-filter">LỌC</button>
-            
-                </div>
-
-                <div className="note-content">
-                  <div className="note-content-header">
-                      <h3 className="title">TIÊU ĐỀ</h3>
-                      <button className="fix">CHỈNH SỬA</button>
-                      <button className="delete">XÓA</button>
-                  </div>
-                  
-                  <h4 className="belong-to">THUỘC VỀ ĐOẠN CHAT NÀO</h4>
-                  <h4 className="start-date">NGÀY TẠO</h4>
-                  <button className="note-link">LINK</button>
-                </div>
-
-
-         </div>
-
-
-        </div>
-
-       
-
+      <div className="fixed top-0 left-0 w-full z-50">
+        <Navbar />
       </div>
 
-       
+      <div className="flex-1 flex flex-col pb-[72px] min-h-0">{children}</div>
 
-
+      <div className="fixed bottom-0 left-0 w-full z-50">
         <Footer/>
+      </div>
     </div>
   );
 }
